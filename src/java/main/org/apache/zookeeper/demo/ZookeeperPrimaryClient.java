2b065501891f3d1f070bd18724e37979698a2b6a
djniexianglin <niexianglin@imdada.cn>
2019-08-29 18:32:41 +0800

    zookeeper init

--- src/java/main/org/apache/zookeeper/demo/ZookeeperPrimaryClient.java
+++ src/java/main/org/apache/zookeeper/demo/ZookeeperPrimaryClient.java
@@ -1,5 +1,6 @@
 package org.apache.zookeeper.demo;
 
+import com.google.common.collect.Lists;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
@@ -10,6 +11,7 @@ import org.apache.zookeeper.server.ZooKeeperServerMain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -42,10 +44,18 @@ public class ZookeeperPrimaryClient {
 
         Stat stat = new Stat();
 
-        byte[] data = zk.getData("/niexianglin", null, stat);
 
-        LOG.info("data is {}",new String(data, Charset.defaultCharset()));
+//        for (int i = 0; i < 128; i++) {
+//            zk.create("/niexianglin"+ i, "121312".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+//
+//            Thread.sleep(100);
+//        }
+//
 
+
+        byte[] data = zk.getData("/niexianglin12", null, new Stat());
+
+        System.out.printf(new String(data, Charset.defaultCharset()));
 
     }
 }
